78a888e38620b743e3f1c9f655f955a337fbdc3e
Igor Pompeo <igor.pompeo1@ibm.com>
2023-05-03 12:26:18 -0300

    Colocando comentários

--- 13.Roman-to-Integer/romanToInt.java
+++ 13.Roman-to-Integer/romanToInt.java
@@ -1,5 +1,7 @@
 class Solution {
     public int romanToInt(String s) {
+        
+        // Crie um mapa que mapeie cada letra romana para seu valor inteiro correspondente.
         Map<Character, Integer> romanToInteger = new HashMap<>();
         romanToInteger.put('I', 1);
         romanToInteger.put('V', 5);
@@ -8,6 +10,8 @@ class Solution {
         romanToInteger.put('C', 100);
         romanToInteger.put('D', 500);
         romanToInteger.put('M', 1000);
+
+        /*Percorra a string do número romano da direita para a esquerda e compare cada letra com a próxima. Se a letra atual for menor do que a próxima, subtraia o valor da letra atual do valor total. Caso contrário, adicione o valor da letra atual ao valor total.*/
         int res = 0;
         for (int i = s.length()-1; i >= 0; i--){
             char c = s.charAt(i);
